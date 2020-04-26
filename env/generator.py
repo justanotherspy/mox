183f75b5faa0e55893a2165eb83b1ad1ea5b2d98
daniel <danielschwar@gmail.com>
2020-04-26 15:25:03 +0200

    Created the GUI shell that needs filling with actions
    The UI is now run from moxui.py it has everything I need to create the application interface. I now need to implement the actions of the generator into it. Might use the generator.py as a seperate module for generating the QR code as well as uploading the file. Need to also look into a good way to make the progress bar representative of accurate feedback.

--- env/generator.py
+++ env/generator.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-# import qrcode
-# import requests
-# import boto3
-# import urllib.parse
+import qrcode
+import requests
+import boto3
+import urllib.parse
 import uuid
 import json
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
 
 access_key = ""
 secret_key = ""
@@ -20,21 +16,10 @@ with open('creds.json') as creds_file:
     secret_key = data['aws_secret_access_key']
     api_key = data['api_key']
 
+
 def main():
 
-    id = str(uuid.uuid4())[:8]
-
-    app = QApplication(sys.argv)
-    widget = QWidget()
-
-    textLabel = QLabel(widget)
-    textLabel.setText("Hello World!")
-    textLabel.move(110,85)
-
-    widget.setGeometry(50,50,320,200)
-    widget.setWindowTitle("PyQt5 Example")
-    widget.show()
-    sys.exit(app.exec_())
+    #id = str(uuid.uuid4())[:8]
 
     # name = input("Name of doll: ")
     # encoded_name = urllib.parse.quote_plus(name)
